258b59e6fd150e80dc087af928d607129361b3de
Eric Lehmer <elehmer@berkeley.edu>
2016-10-13 09:51:55 -0700

    Changes to DB connection

--- ceqa/settings.py
+++ ceqa/settings.py
@@ -17,9 +17,8 @@ DATABASES = {
         'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'ceqanet',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'postgres',
-        'PASSWORD': 'FuckYouPostgres',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'USER': 'ceqa',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '5432',                      # Set to empty string for default.
     }
 }
